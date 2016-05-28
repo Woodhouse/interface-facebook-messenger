c1953321d08834d276ad1efd81a7e85f960c9ff0
Luke Bonaccorsi <luke@lukeb.co.uk>
2016-05-29 00:08:02 +0100

    Initial commit for facebook messenger interface

---
+++ index.js
@@ -0,0 +1,53 @@
+var bluebird = require('bluebird'),
+    messenger = bluebird.promisify(require('facebook-chat-api')),
+    facebook = function(){
+        this.name = 'facebook';
+        this.displayname = 'Facebook Messenger';
+        this.description = 'Send messages to woodhouse via Facebook Messenger';
+
+        this.defaultPrefs = [{
+            name: 'username',
+            displayname: 'Username',
+            type: 'text',
+            value: ''
+        },{
+            name: 'password',
+            displayname: 'Password',
+            type: 'password',
+            value: ''
+        }];
+    };
+
+facebook.prototype.init = function(){
+    this.getPrefs().done(function(prefs){
+        this.prefs = prefs;
+        this.connect();
+    }.bind(this));
+}
+
+facebook.prototype.connect = function() {
+    messenger({
+        email: this.prefs.username,
+        password: this.prefs.password
+    }, {
+        logLevel: 'silent'
+    }).then(function(api) {
+        this.api = bluebird.promisifyAll(api);
+
+        this.api.listenAsync().then(function(message) {
+            this.messageRecieved(message.threadID, message.body, message.senderID);
+        }.bind(this));
+
+        this.addMessageSender(function(message, to) {
+            this.api.sendMessage(message, to);
+        }.bind(this));
+    }.bind(this));
+}
+
+facebook.prototype.exit = function(){
+    if (this.api) {
+        this.api.logout();
+    }
+}
+
+module.exports = facebook;
